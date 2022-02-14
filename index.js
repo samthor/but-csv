8503b2fb7bfec31f4be26aa76d463d3ff1b08836
Sam Thorogood <sam@localhost>
2022-02-15 09:23:41 +1100

    755

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const C_QUOTE = 34;
  * @param {Uint8Array|string} source
  * @return {string[][]}
  */
-export function parse(source) {
+export const parse = (source) => {
   return [...iter(source)];
 }
 
@@ -29,11 +29,14 @@ export function *iter(source) {
   /** @type {string[]} */
   const row = [];
 
-  outer: for (; ;) {
+  for (; ;) {
     if (i >= source.length) {
       yield row;
       break;
     }
+
+    // we consume at most one col per outer loop
+    let s = '';
 
     const start = source[i];
     switch (start) {
@@ -45,61 +48,54 @@ export function *iter(source) {
       }
 
       case C_QUOTE: {
-        let s = '';
-
+        // consume many parts of quoted string
         for (; ;) {
-          // consume quoted string
-          const next = source.indexOf(C_QUOTE, i + 1);
+          let next = source.indexOf(C_QUOTE, i + 1);
           if (next === -1) {
-            // emit to end
-            const s = dec(source.subarray(i + 1));
-            i = source.length;
-            row.push(s);
-            yield row;
-            return;
+            next = source.length;
           }
+
           const part = dec(source.subarray(i + 1, next));
           s += part;
 
           i = next + 1;
           if (i >= source.length) {
-            break;
+            break;  // end of input
           }
           const check = source[i];
           if (check === C_COMMA || check === C_NEWLINE) {
-            break;
+            break;  // end of string
           } else if (check !== C_QUOTE) {
-            --i;  // don't prevent missing double quote
+            --i;  // allow missing double quote _anyway_
           }
           s += '"';
         }
 
-        row.push(s);
         break;
       }
 
       default: {
-        // guess that it's probably , next (not quoted)
-        let comma = source.indexOf(C_COMMA, i);
-        if (comma === -1) {
-          comma = source.length;
+        // this is a "normal" value, ends with a comma or newline
+        // look for comma first (educated guess)
+        let to = source.indexOf(C_COMMA, i);
+        if (to === -1) {
+          to = source.length;
         }
-        let to = comma;
 
-        let newline = source.subarray(i, comma).indexOf(C_NEWLINE);
+        let newline = source.subarray(i, to).indexOf(C_NEWLINE);
         if (newline === -1) {
           // ignore, ok
         } else {
           to = newline + i;
         }
 
-        const value = dec(source.subarray(i, to));
-        row.push(value);
-
+        s = dec(source.subarray(i, to));
         i = to;
         break;
       }
     }
+
+    row.push(s);
 
     // look for ,
     if (source[i] === C_COMMA) {
@@ -117,7 +113,7 @@ const globalQuote = /"/g;
 /**
  * @param {string} raw
  */
-export const r = (raw) => {
+const r = (raw) => {
   if (!needsQuoteRegexp.test(raw)) {
     return raw;
   }
@@ -128,6 +124,6 @@ export const r = (raw) => {
 /**
  * @param {string[][]} raw
  */
-export function build(raw) {
+export const build = (raw) => {
   return raw.map((row) => row.map(r).join(',')).join('\n');
 }
