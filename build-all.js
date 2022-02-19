5552c773b8730a9f7d621b53b7ac27555505c77d
Sam Thorogood <sam@localhost>
2022-02-20 09:49:13 +1100

    show stat in build

--- build-all.js
+++ build-all.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
 import * as childProcess from 'child_process';
+import * as fs from 'fs';
 
 const scripts = ['index'];
 for (const script of scripts) {
   childProcess.execSync(`esbuild --format=esm --bundle --minify --tree-shaking=true ${script}.js > ${script}.min.mjs`);
+
+  const stat = fs.statSync(`${script}.min.mjs`);
+  console.info(script, '=>', stat.size);
 }
